bdce903f9db11c18268830c9e75eb62e919396f7
ganm0r <gndhrvmv@gmail.com>
2021-08-31 13:15:19 +0530

    Home Page Updated 🚀

--- src/home/home.js
+++ src/home/home.js
@@ -1,18 +1,30 @@
-dataset.onchange = function(ev) {
+dataset.onchange = (ev) => {
+    try {
+        validateInputFile(ev)
+    }
+    catch (err) {
+        console.error(err)
+    }
+};
+
+function validateInputFile (ev) {
+    let inputFile = ev.target.files[0];
     
-    ev.preventDefault();
-
-    var file = this.files[0];
-
-    if(file.type.indexOf('.csv') !== 0) {
-        console.log('[FILE FETCH]: Fail');
-        if(!(alert('Error: Invalid File Type'))) {
-            window.location.reload();
-        }
+    if (!(inputFile.type === 'text/csv')) {
+        alert('Error: Invalid File Type');
+        throw '[FILE FETCH]: Fail';
     }
     else {
-        //Add what to do with the input file
+        storeInputBlobAsFile(inputFile, inputFile.name);
+        console.log('[FILE FETCH]: Success');
+        console.log('Redirecting...');
+        window.location.replace("/src/visualiser/visualiser.html");
     }
 }
 
+function storeInputBlobAsFile(csvFile, fileName) {
+    csvFile.lastModifiedDate = new Date();
+    csvFile.name = fileName;
+    return csvFile;
+}
 
--- src/visualiser/visualiser.js
+++
@@ -1,6 +0,0 @@
-dfd.read_csv("https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/airquality.csv")
-    .then(df => {
-        df.plot(document.getElementById('scatterchart')).scatter({ x: 'Day', y: 'Temp'})
-    }).catch(e => {
-        console.log(e);
-    })
