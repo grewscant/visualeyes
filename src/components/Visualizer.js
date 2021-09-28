ef652de4f5bd3db63b88974daca493e0ce196a7e
ganm0r <gndhrvmv@gmail.com>
2021-09-28 14:36:05 +0530

    :sparkles: Add available plot names

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -26,20 +26,29 @@ const Visualizer = ({ props }) => {
       <div className="vis-container">
         <h1 className="title">Visualization</h1>
         <div className="vis-btn-container">
-          <input type="radio" name="vis-select" id="vis-lorem" />
-          <label htmlFor="vis-lorem">Lorem.</label>
+          <input type="radio" name="vis-select" id="vis-line" />
+          <label htmlFor="vis-line">Line Chart</label>
 
-          <input type="radio" name="vis-select" id="vis-asperiores" />
-          <label htmlFor="vis-asperiores">Asperiores.</label>
+          <input type="radio" name="vis-select" id="vis-bar" />
+          <label htmlFor="vis-bar">Bar Chart</label>
 
-          <input type="radio" name="vis-select" id="vis-ducimus" />
-          <label htmlFor="vis-ducimus">Ducimus.</label>
+          <input type="radio" name="vis-select" id="vis-scatter" />
+          <label htmlFor="vis-scatter">Scatter Plot</label>
 
-          <input type="radio" name="vis-select" id="vis-adipisci" />
-          <label htmlFor="vis-adipisci">Adipisci.</label>
+          <input type="radio" name="vis-select" id="vis-histogram" />
+          <label htmlFor="vis-histogram">Histogram</label>
 
-          <input type="radio" name="vis-select" id="vis-maiores" />
-          <label htmlFor="vis-maiores">Maiores.</label>
+          <input type="radio" name="vis-select" id="vis-pie" />
+          <label htmlFor="vis-pie">Pie Chart</label>
+
+          <input type="radio" name="vis-select" id="vis-box" />
+          <label htmlFor="vis-box">Box Plot</label>
+
+          <input type="radio" name="vis-select" id="vis-violin" />
+          <label htmlFor="vis-violin">Violin Plot</label>
+
+          <input type="radio" name="vis-select" id="vis-time" />
+          <label htmlFor="vis-time">Timeseries Plot</label>
         </div>
         <div className="vis-plot-container"></div>
       </div>
